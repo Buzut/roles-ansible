42655ad6c3332ba0a6263ccca539319ccbdc1332
Buzut <blog@buzut.fr>
2018-01-29 19:47:49 +0100

    Accept outgoing trafic by default

--- roles/common/files/iptables.sh
+++ roles/common/files/iptables.sh
@@ -13,21 +13,21 @@ ip6tables -X
 # DROP POLICY
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
+iptables -P OUTPUT ACCEPT
 
 # v6
 ip6tables -P INPUT DROP
 ip6tables -P FORWARD DROP
-ip6tables -P OUTPUT DROP
+ip6tables -P OUTPUT ACCEPT
 
 #######################################################################
 
 # INPUT
 # DOESN'T BREAK ESTABLISHED CONNECTIONS
-iptables -A INPUT -m conntrack --ctstate -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # v6
-ip6tables -A INPUT -m conntrack --ctstate -j ACCEPT
+ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 #######################################################################
 
@@ -48,31 +48,3 @@ ip6tables -A  OUTPUT -o lo -j ACCEPT
 
 #######################################################################
 
-# OUTPUT
-# DOESN'T BREAK ESTABLISHED CONNECTIONS
-iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-# v6
-ip6tables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-#######################################################################
-
-# SSH, HTTP, SMTP, DNS, DHCP, NTP, ping
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport smtp -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-iptables -I OUTPUT -p udp --dport 67:68 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
-iptables -A OUTPUT -p icmp -j ACCEPT
-
-# v6
-ip6tables -A OUTPUT -p tcp --dport 80 -j ACCEPT
-ip6tables -A OUTPUT -p tcp --dport 443 -j ACCEPT
-ip6tables -A OUTPUT -p tcp --dport smtp -j ACCEPT
-ip6tables -A OUTPUT -p udp --dport 53 -j ACCEPT
-ip6tables -A OUTPUT -p udp --dport 123 -j ACCEPT
-ip6tables -A OUTPUT -p ipv6-icmp -j ACCEPT
-
-#######################################################################
